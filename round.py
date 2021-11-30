3d28327d0eb35c14716b0ac4644c087d58350e38
bansheerubber <gary.cactus.email@gmail.com>
2021-11-29 21:55:48 -0700

    skip over problems if we encounter them in round.py

--- round.py
+++ round.py
@@ -6,19 +6,29 @@ import time
 nodes_extractor = re.compile(r"expanded: ([0-9]+)")
 cost_extractor = re.compile(r"cost of ([0-9]+)")
 def extract_results(output):
-	nodes = nodes_extractor.search(output).group(1)
-	cost = cost_extractor.search(output).group(1)
+	match1 = nodes_extractor.search(output)
+	if match1:
+		nodes = match1.group(1)
+	else:
+		return None
+
+	match2 = cost_extractor.search(output)
+	if match2:
+		cost = match2.group(1)
+	else:
+		return None
 	return (nodes, cost)
 
 def write_results(result, search, filename):
-	file = open(f"./results/{search}.stats.{filename}", "a")
-	file.write(f"{result[0]},{result[1]}\n")
-	file.close()
+	if result != None:
+		file = open(f"./results/{search}.stats.{filename}", "a")
+		file.write(f"{result[0]},{result[1]}\n")
+		file.close()
 
 print(f"Using program '{sys.argv[1]}' to generate maze to file './layouts/{sys.argv[2]}.lay', outputting results to './results/*.stats.{sys.argv[3]}'")
 time.sleep(1)
 
-for _ in range(0, 10000): # run 10000 times, adjust this if needed
+for _ in range(0, 2500): # run 10000 times, adjust this if needed
 	maze = os.popen(f"python {sys.argv[1]}").read() # randomly generate the maze
 	file = open(f"./layouts/{sys.argv[2]}.lay", "w")
 	file.write(maze)
