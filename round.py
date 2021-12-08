36e5cb81c31d0bcf21f79c32a80623c7ed10bf8b
bansheerubber <gary.cactus.email@gmail.com>
2021-12-07 20:02:52 -0700

    add corners problem support to round.py

--- round.py
+++ round.py
@@ -28,6 +28,10 @@ def write_results(result, search, filename):
 print(f"Using program '{sys.argv[1]}' to generate maze to file './layouts/{sys.argv[2]}.lay', outputting results to './results/*.stats.{sys.argv[3]}'")
 time.sleep(1)
 
+corners = False
+if "corners" in sys.argv[1]:
+	corners = True
+
 for _ in range(0, 2500): # run 10000 times, adjust this if needed
 	maze = os.popen(f"python {sys.argv[1]}").read() # randomly generate the maze
 	file = open(f"./layouts/{sys.argv[2]}.lay", "w")
@@ -35,11 +39,18 @@ for _ in range(0, 2500): # run 10000 times, adjust this if needed
 	file.close()
 
 	# run the searches
-	dfs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=dfs -q").read())
-	bfs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=bfs -q").read())
-	ucs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=ucs -q").read())
-	astar = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic -q").read())
-	bi = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=bi,heuristic=manhattanHeuristic -q").read())
+	if not corners:
+		dfs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=dfs -q").read())
+		bfs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=bfs -q").read())
+		ucs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=ucs -q").read())
+		astar = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic -q").read())
+		bi = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=bi,heuristic=manhattanHeuristic -q").read())
+	else:
+		dfs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=dfs,prob=CornersProblem -q").read())
+		bfs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=bfs,prob=CornersProblem -q").read())
+		ucs = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p SearchAgent -a fn=ucs,prob=CornersProblem -q").read())
+		astar = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p AStarCornersAgent -q").read())
+		bi = extract_results(os.popen(f"python pacman.py -l {sys.argv[2]} -z .5 -p BiDirectionalCornersAgent -q").read())
 
 	print(dfs, bfs, ucs, astar, bi)
 
